dd5af6d0ac55a35c587dacc9905f041641e38b5e
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-11-16 15:57:12 +0400

    header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,5 @@
 <template>
-      <nav class="navbar navbar-expand-lg sticky-top w-100 bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg sticky-top w-100 ">
         <div class="container-fluid">
           <router-link class="navbar-brand" to="/">{{ $t('amazon') }}</router-link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
