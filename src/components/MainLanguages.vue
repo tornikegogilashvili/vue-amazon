39ea67b19237c5f4d83209c118fa1ab3bcf8f8bb
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-11-18 19:25:11 +0400

    language flags

--- src/components/MainLanguages.vue
+++ src/components/MainLanguages.vue
@@ -1,10 +1,14 @@
 <template>
     <ul class="list-inline mb-0 ms-4">
         <li class="list-inline-item">
-            <button @click="changeLang('ge')" class="btn btn-secondary">GE</button>
+            <button @click="changeLang('ge')" class="btn">
+                <img class="flag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flag_of_Georgia.svg/1280px-Flag_of_Georgia.svg.png" alt="Geo">
+            </button>
         </li>
         <li class="list-inline-item">
-            <button @click="changeLang('en')" class="btn btn-secondary">EN</button>
+            <button @click="changeLang('en')" class="btn">
+                <img class="flag" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png" alt="GBR">
+            </button>
         </li>
     </ul>
 </template>
@@ -17,3 +21,10 @@ export default {
     }
 }
 </script>
+
+<style>
+    .flag{
+        width: 30px;
+        height: 20px;
+    }
+</style>
